39f39e28e8440a840e4e41b606508aa4eb1045ac
khurshidkiran7 <khurshid.kiran7@gmail.com>
2025-08-28 01:23:14 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+function openCV() {
+  window.open("pdfs/Resume.pdf", "_blank"); // open in new tab
+}
+function opennewLetterPDF() {
+  window.open("pdfs/Newsletter-DGME.pdf", "_blank");
+}
+function openannualreportPDF() {
+  window.open("pdfs/AnnualReport-DGME-2016-17.pdf", "_blank");
+}
+function openDSpace() {
+  window.open(
+    "http://repository.pastic.gov.pk/jspui/handle/123456789/5910",
+    "_blank"
+  );
+}
